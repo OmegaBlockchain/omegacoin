3b2aae5cd9147ff03b65b44b73c335869fdd9cff
super7coin <34923433+super7coin@users.noreply.github.com>
2018-01-18 10:36:56 -0600

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -93,7 +93,7 @@ public:
         consensus.BIP34Hash = uint256S("0x000007d91d1254d60e2dd1ae580383070a4ddffa4c64c2eeb4a2f9ecc0414343");
         consensus.powLimit = uint256S("00000fffff000000000000000000000000000000000000000000000000000000");
         consensus.nPowTargetTimespan = 120; // Super7Coin: 1 day
-        consensus.nPowTargetSpacing = 60; // Super7Coin: 2.5 minutes
+        consensus.nPowTargetSpacing = 1; // Super7Coin: 2.5 minutes
         consensus.fPowAllowMinDifficultyBlocks = false;
         consensus.fPowNoRetargeting = false;
         //consensus.nPowKGWHeight = 15200;

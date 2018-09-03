marcusfox0/polis

Implement and enforce CbTx with correct block height and deprecate BIP34

src/evo/cbtx.h
@@ -1,0 +1,41 @@
+// Copyright (c) 2017 The Dash Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef DASH_CBTX_H
+#define DASH_CBTX_H
+
+#include "primitives/transaction.h"
+#include "consensus/validation.h"
+
+class CBlockIndex;
+class UniValue;
+
+// coinbase transaction
+class CCbTx
+{
+public:
+    static const int CURRENT_VERSION = 1;
+
+public:
+    uint16_t nVersion{CURRENT_VERSION};
+    int32_t height{0};
+    uint256 merkleRootMNList;
+
+public:
+    ADD_SERIALIZE_METHODS;
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action)
+    {
+        READWRITE(height);
+        READWRITE(merkleRootMNList);
+    }
+
+    std::string ToString() const;
+    void ToJson(UniValue& obj) const;
+};
+
+bool CheckCbTx(const CTransaction& tx, const CBlockIndex* pindex, CValidationState& state);
+
+#endif//DASH_CBTX_H

src/evo/specialtx.cpp
@@ -13,6 +13,7 @@
 #include "specialtx.h"
 #include "providertx.h"
 #include "deterministicmns.h"
+#include "cbtx.h"
 
 bool CheckSpecialTx(const CTransaction& tx, const CBlockIndex* pindex, CValidationState& state)
 {
@@ -34,6 +35,8 @@ bool CheckSpecialTx(const CTransaction& tx, const CBlockIndex* pindex, CValidati
             return CheckProUpRegTx(tx, pindex, state);
         case TRANSACTION_PROVIDER_UPDATE_REVOKE:
             return CheckProUpRevTx(tx, pindex, state);
+        case TRANSACTION_COINBASE:
+            return CheckCbTx(tx, pindex, state);
     }
 
     return state.DoS(100, false, REJECT_INVALID, "bad-tx-type");
@@ -50,6 +53,8 @@ bool ProcessSpecialTx(const CTransaction& tx, const CBlockIndex* pindex, CValida
         case TRANSACTION_PROVIDER_UPDATE_REGISTRAR:
         case TRANSACTION_PROVIDER_UPDATE_REVOKE:
             return true; // handled in batches per block
+        case TRANSACTION_COINBASE:
+            return true; // nothing to do
     }
 
     return state.DoS(100, false, REJECT_INVALID, "bad-tx-type");
@@ -66,6 +71,8 @@ bool UndoSpecialTx(const CTransaction& tx, const CBlockIndex* pindex)
         case TRANSACTION_PROVIDER_UPDATE_REGISTRAR:
         case TRANSACTION_PROVIDER_UPDATE_REVOKE:
             return true; // handled in batches per block
+        case TRANSACTION_COINBASE:
+            return true; // nothing to do
     }
 
     return false;

src/miner.cpp
@@ -28,6 +28,9 @@
 #include "masternode-sync.h"
 #include "validationinterface.h"
 
+#include "evo/specialtx.h"
+#include "evo/cbtx.h"
+
 #include <algorithm>
 #include <boost/thread.hpp>
 #include <boost/tuple/tuple.hpp>
@@ -126,6 +129,9 @@ std::unique_ptr<CBlockTemplate> BlockAssembler::CreateNewBlock(const CScript& sc
     pblocktemplate->vTxSigOps.push_back(-1); // updated at end
 
     LOCK2(cs_main, mempool.cs);
+
+    bool fDIP0003Active_context = VersionBitsState(chainActive.Tip(), chainparams.GetConsensus(), Consensus::DEPLOYMENT_DIP0003, versionbitscache) == THRESHOLD_ACTIVE;
+
     CBlockIndex* pindexPrev = chainActive.Tip();
     nHeight = pindexPrev->nHeight + 1;
 
@@ -166,7 +172,19 @@ std::unique_ptr<CBlockTemplate> BlockAssembler::CreateNewBlock(const CScript& sc
 
     // Compute regular coinbase transaction.
     coinbaseTx.vout[0].nValue = blockReward;
-    coinbaseTx.vin[0].scriptSig = CScript() << nHeight << OP_0;
+
+    if (!fDIP0003Active_context) {
+        coinbaseTx.vin[0].scriptSig = CScript() << nHeight << OP_0;
+    } else {
+        coinbaseTx.vin[0].scriptSig = CScript() << OP_RETURN;
+
+        coinbaseTx.nVersion = 3;
+        coinbaseTx.nType = TRANSACTION_COINBASE;
+
+        CCbTx cbTx;
+        cbTx.height = nHeight;
+        SetTxPayload(coinbaseTx, cbTx);
+    }
 
     // Update coinbase transaction with additional info about masternode and governance payments,
     // get some info back to pass to getblocktemplate

OmegaBlockchain/omegacoin

src/evo/cbtx.h
@@ -1,0 +1,42 @@
+// Copyright (c) 2017 The Dash Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef DASH_CBTX_H
+#define DASH_CBTX_H
+
+#include "primitives/transaction.h"
+#include "consensus/validation.h"
+
+class CBlockIndex;
+class UniValue;
+
+// coinbase transaction
+class CCbTx
+{
+public:
+    static const uint16_t CURRENT_VERSION = 1;
+
+public:
+    uint16_t nVersion{CURRENT_VERSION};
+    int32_t nHeight{0};
+    uint256 merkleRootMNList;
+
+public:
+    ADD_SERIALIZE_METHODS;
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action)
+    {
+        READWRITE(nVersion);
+        READWRITE(nHeight);
+        READWRITE(merkleRootMNList);
+    }
+
+    std::string ToString() const;
+    void ToJson(UniValue& obj) const;
+};
+
+bool CheckCbTx(const CTransaction& tx, const CBlockIndex* pindexPrev, CValidationState& state);
+
+#endif//DASH_CBTX_H

src/evo/specialtx.cpp
@@ -12,6 +12,7 @@
 
 #include "specialtx.h"
 #include "deterministicmns.h"
+#include "cbtx.h"
 
 bool CheckSpecialTx(const CTransaction& tx, const CBlockIndex* pindexPrev, CValidationState& state)
 {
@@ -33,6 +34,8 @@ bool CheckSpecialTx(const CTransaction& tx, const CBlockIndex* pindexPrev, CVali
             return CheckProUpRegTx(tx, pindexPrev, state);
         case TRANSACTION_PROVIDER_UPDATE_REVOKE:
             return CheckProUpRevTx(tx, pindexPrev, state);
+        case TRANSACTION_COINBASE:
+            return CheckCbTx(tx, pindexPrev, state);
     }
 
     return state.DoS(10, false, REJECT_INVALID, "bad-tx-type");
@@ -49,6 +52,8 @@ bool ProcessSpecialTx(const CTransaction& tx, const CBlockIndex* pindex, CValida
         case TRANSACTION_PROVIDER_UPDATE_REGISTRAR:
         case TRANSACTION_PROVIDER_UPDATE_REVOKE:
             return true; // handled in batches per block
+        case TRANSACTION_COINBASE:
+            return true; // nothing to do
     }
 
     return state.DoS(100, false, REJECT_INVALID, "bad-tx-type");
@@ -65,6 +70,8 @@ bool UndoSpecialTx(const CTransaction& tx, const CBlockIndex* pindex)
         case TRANSACTION_PROVIDER_UPDATE_REGISTRAR:
         case TRANSACTION_PROVIDER_UPDATE_REVOKE:
             return true; // handled in batches per block
+        case TRANSACTION_COINBASE:
+            return true; // nothing to do
     }
 
     return false;

src/miner.cpp
@@ -28,6 +28,9 @@
 #include "masternode-sync.h"
 #include "validationinterface.h"
 
+#include "evo/specialtx.h"
+#include "evo/cbtx.h"
+
 #include <algorithm>
 #include <boost/thread.hpp>
 #include <boost/tuple/tuple.hpp>
@@ -126,6 +129,9 @@ std::unique_ptr<CBlockTemplate> BlockAssembler::CreateNewBlock(const CScript& sc
     pblocktemplate->vTxSigOps.push_back(-1); // updated at end
 
     LOCK2(cs_main, mempool.cs);
+
+    bool fDIP0003Active_context = VersionBitsState(chainActive.Tip(), chainparams.GetConsensus(), Consensus::DEPLOYMENT_DIP0003, versionbitscache) == THRESHOLD_ACTIVE;
+
     CBlockIndex* pindexPrev = chainActive.Tip();
     nHeight = pindexPrev->nHeight + 1;
 
@@ -166,7 +172,19 @@ std::unique_ptr<CBlockTemplate> BlockAssembler::CreateNewBlock(const CScript& sc
 
     // Compute regular coinbase transaction.
     coinbaseTx.vout[0].nValue = blockReward;
-    coinbaseTx.vin[0].scriptSig = CScript() << nHeight << OP_0;
+
+    if (!fDIP0003Active_context) {
+        coinbaseTx.vin[0].scriptSig = CScript() << nHeight << OP_0;
+    } else {
+        coinbaseTx.vin[0].scriptSig = CScript() << OP_RETURN;
+
+        coinbaseTx.nVersion = 3;
+        coinbaseTx.nType = TRANSACTION_COINBASE;
+
+        CCbTx cbTx;
+        cbTx.nHeight = nHeight;
+        SetTxPayload(coinbaseTx, cbTx);
+    }
 
     // Update coinbase transaction with additional info about masternode and governance payments,
     // get some info back to pass to getblocktemplate
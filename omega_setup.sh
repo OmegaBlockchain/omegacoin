7b31e6e7e5fe00b74459f13ce898167a12d94a93
RottenCoin <38838284+RottenCoin@users.noreply.github.com>
2020-06-01 18:54:04 +0100

    allowing unauthenticated download of libdb4.8

--- omega_setup.sh
+++ omega_setup.sh
@@ -10,14 +10,14 @@ if [[ $DOSETUP =~ "y" ]] ; then
   sudo apt-get install -y nano htop git curl
   sudo apt-get install -y software-properties-common
   sudo apt-get install -y build-essential libtool autotools-dev pkg-config libssl-dev
-  sudo apt-get install -y libboost-all-dev libzmq3-dev
+  sudo apt-get install -y libboost-all-dev libzmq3-dev libzmq5
   sudo apt-get install -y libevent-dev
   sudo apt-get install -y libminiupnpc-dev
   sudo apt-get install -y autoconf
   sudo apt-get install -y automake unzip
   sudo add-apt-repository  -y  ppa:bitcoin/bitcoin
   sudo apt-get update
-  sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
+  sudo apt-get install -y libdb4.8-dev libdb4.8++-dev --allow-unauthenticated
 
   cd /var
   sudo touch swap.img

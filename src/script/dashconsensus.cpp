dashpay/dash

Merge #13655: bitcoinconsensus: invalid flags error should be set to bitcoinconsensus_err
417b6c1d2990ffc78c029442e027797d724a101f bitcoinconsensus: invalid flags should be set to bitcoinconsensus_error type, add test cases covering bitcoinconsensus error codes (Thomas Kerin)

Pull request description:

  A check was added to the bitcoinconsensus verify_script codepath to ensure that callers only used _exposed_ interpreter flags. I think this error should be written to `bitcoinconsensus_err* err` and not returned by verify_script?

  I modified the check so it indicates the error using *err like the others, and added tests covering the error codes.

Tree-SHA512: 8ab370e56956a7d4740f83475e6078774affd663ac92383a02b85295da550f1b4f7a7a68f32ed5c5bcb39d98e2f15ec0b76de8399887e7763eb7c1e21d131093

src/script/dashconsensus.cpp
@@ -81,7 +81,7 @@ int dashconsensus_verify_script(const unsigned char *scriptPubKey, unsigned int 
                                     unsigned int nIn, unsigned int flags, dashconsensus_error* err)
 {
     if (!verify_flags(flags)) {
-        return dashconsensus_ERR_INVALID_FLAGS;
+        return set_error(err, bitcoinconsensus_ERR_INVALID_FLAGS);
     }
     try {
         TxInputStream stream(SER_NETWORK, PROTOCOL_VERSION, txTo, txToLen);

OmegaBlockchain/omegacoin

src/script/dashconsensus.cpp
@@ -81,7 +81,7 @@ int dashconsensus_verify_script(const unsigned char *scriptPubKey, unsigned int 
                                     unsigned int nIn, unsigned int flags, dashconsensus_error* err)
 {
     if (!verify_flags(flags)) {
-        return dashconsensus_ERR_INVALID_FLAGS;
+        return set_error(err, dashconsensus_ERR_INVALID_FLAGS);
     }
     try {
         TxInputStream stream(SER_NETWORK, PROTOCOL_VERSION, txTo, txToLen);
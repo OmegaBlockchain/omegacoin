syscoin/syscoin

Merge #18692: test: Bump timeout in wallet_import_rescan
fabfcad8764bb8f807b0ac5f3482b414278a4525 test: Bump timeout in wallet_import_rescan (MarcoFalke)

Pull request description:

  Avoid timeouts when starting the node, also make error message more verbose

ACKs for top commit:
  practicalswift:
    ACK fabfcad8764bb8f807b0ac5f3482b414278a4525 -- patch looks correct

Tree-SHA512: 8fd60a05380349f521d0e814d2f268702dfbe57c7567a4f6e94435498dfdd32909179d75fded44757ecb1a93a4045842bc6d00bfd6cd18ba751513461359c7b0

test/functional/test_framework/test_node.py
@@ -241,7 +241,7 @@ class TestNode():
                 if "No RPC credentials" not in str(e):
                     raise
             time.sleep(1.0 / poll_per_s)
-        self._raise_assertion_error("Unable to connect to syscoind")
+        self._raise_assertion_error("Unable to connect to syscoind after {}s".format(self.rpc_timeout))
 
     def generate(self, nblocks, maxtries=1000000):
         self.log.debug("TestNode.generate() dispatches `generate` call to `generatetoaddress`")
@@ -527,7 +527,6 @@ def arg_to_cli(arg):
 
 class TestNodeCLI():
     """Interface to syscoin-cli for an individual node"""
-
     def __init__(self, binary, datadir):
         self.options = []
         self.binary = binary

OmegaBlockchain/omegacoin

test/functional/test_framework/test_node.py
@@ -274,7 +274,7 @@ class TestNode():
                 if "No RPC credentials" not in str(e):
                     raise
             time.sleep(1.0 / poll_per_s)
-        self._raise_assertion_error("Unable to connect to dashd")
+        self._raise_assertion_error("Unable to connect to dashd after {}s".format(self.rpc_timeout))
 
     def generate(self, nblocks, maxtries=1000000):
         self.log.debug("TestNode.generate() dispatches `generate` call to `generatetoaddress`")
@@ -570,7 +570,6 @@ def arg_to_cli(arg):
 
 class TestNodeCLI():
     """Interface to dash-cli for an individual node"""
-
     def __init__(self, binary, datadir):
         self.options = []
         self.binary = binary